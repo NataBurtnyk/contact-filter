7cf2633a82b947ee1fa1d164855a51973bfb396b
Natalia Burtnyk <nataliaburtnyk@gmail.com>
2020-02-12 21:36:34 +0200

    First commit.

---
+++ src/main/java/com/contactfilter/ContactFilterApplication.java
@@ -0,0 +1,13 @@
+package com.contactfilter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactFilterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContactFilterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/contactfilter/ContactFilterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contactfilter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactFilterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
